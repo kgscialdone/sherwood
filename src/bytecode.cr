009f84296707c3a8d4197c62f41ad5636abeb8aa
Connor Scialdone <cj@swordglowsblue.com>
2019-02-12 17:02:21 -0700

    Implement control flow opcodes

--- src/bytecode.cr
+++ src/bytecode.cr
@@ -7,14 +7,20 @@ end
 
 class Bytecode < Array(Opcode)
   getter raw
+  getter labels = {} of String => Int32
+
   def initialize(@raw : Array(Byte))
     super()
     insp = 0
 
     while curbyte = @raw[insp]?
-      (size = getCurInstWidth(insp, @raw)) > 0 && 
-        self << Opcode.new(curbyte, @raw[insp+1...insp+size]) || 
-        self << Opcode.new(curbyte)
+      op = (size = getCurInstWidth(insp, @raw)) > 0 ? 
+        Opcode.new(curbyte, @raw[insp+1...insp+size]) :
+        Opcode.new(curbyte)
+      case curbyte
+        when 0x70 then self.labels[op.data.bitwiseString] = self.size
+        else self << op
+      end
       insp += size
     end
   end
@@ -24,7 +30,7 @@ class Bytecode < Array(Opcode)
     when 0x01, 0x02 then 1
     when 0x03, 0x05, 0x07 then 4
     when 0x04, 0x06, 0x08 then 8
-    when 0x09 then 4+prog[insp+1..insp+4].bitwiseConcat
+    when 0x09, 0x70, 0x71, 0x72 then 4+prog[insp+1..insp+4].bitwiseConcat
     when 0x0a then 4
     else 0
     end
